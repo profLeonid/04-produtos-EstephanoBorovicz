d50acd50400945a6d113d0ef21320ca07a331038
ESTEPHANO BOROVICZ BISNETO <25203672@dsn127m1249386.cfp127aluno.local>
2026-02-24 14:58:47 -0300

    entrega

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 'use strict'
 
 function adicionarProdutos(){
-    console.log("Adicionando produtos...")
+    const produto = document.getElementById('produto')
+    const lista = document.getElementById('lista')
+
+    const span = document.createElement('span')
+    span.textContent = produto.value
+    lista.appendChild(span)
+    span.className = 'bg-blue-200 px-8 py-2'
 }
