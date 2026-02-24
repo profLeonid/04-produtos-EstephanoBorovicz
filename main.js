f20076b2023791c4ab5a8b7f1e2d93a22bc993d7
ESTEPHANO BOROVICZ BISNETO <25203672@dsn127m1249386.cfp127aluno.local>
2026-02-24 17:01:55 -0300

    quase funcionando

--- main.js
+++ main.js
@@ -1,11 +1,67 @@
 'use strict'
 
-function adicionarProdutos(){
-    const produto = document.getElementById('produto')
-    const lista = document.getElementById('lista')
+function apertarBotao(){
+    
+        adicionarCodigos()
+        adicionarProdutos()
+        adicionarQuantidades()
+        limparBotao()
+}
 
-    const span = document.createElement('span')
-    span.textContent = produto.value
-    lista.appendChild(span)
-    span.className = 'bg-blue-200 px-8 py-2'
+
+function adicionarCodigos(){
+    const codigo = document.getElementById('inputCodigo')
+
+    if(codigo == ""){
+        return false
+    }else{
+    const listaCodigos = document.getElementById('listaCodigos')
+    const spanCodigo = document.createElement('spanCodigo')
+    
+    spanCodigo.textContent = codigo.value
+    listaCodigos.appendChild(spanCodigo)
+    spanCodigo.className = 'bg-blue-200 px-8 py-2'
+
+    return codigo
+    }
+}
+
+function adicionarProdutos(){
+    const produto = document.getElementById('inputProduto')
+    if (produto ==""){
+        return false
+    }else{
+    const listaProdutos = document.getElementById('listaProdutos')
+    const spanProduto = document.createElement('spanProduto')
+
+    spanProduto.textContent = produto.value
+    listaProdutos.appendChild(spanProduto)
+    spanProduto.className = 'bg-blue-200 px-8 py-2'
+    return produto
+    }
+}
+
+function adicionarQuantidades(){
+    const quantidade = document.getElementById('inputQuantidade')
+
+    if(quantidade==""){
+        return false
+        }else{
+    
+    const listaQuantidades = document.getElementById('listaQuantidades')
+    const spanQuantidade = document.createElement('spanQuantidade')
+
+    spanQuantidade.textContent = quantidade.value
+    listaQuantidades.appendChild(spanQuantidade)
+    spanQuantidade.className = 'bg-blue-200 px-8 py-2'
+    return quantidade
+    }
+}
+
+
+
+function limparBotao(){
+    document.getElementById('inputCodigo').value = ""
+    document.getElementById('inputProduto').value = ""
+    document.getElementById('inputQuantidade').value = ""
 }
